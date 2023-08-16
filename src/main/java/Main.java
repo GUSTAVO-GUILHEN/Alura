311139fd474627d11a79c60dcd32ec4ab0fea030
GUSTAVO-GUILHEN <gguilhen98@gmail.com>
2023-08-15 22:46:12 -0300

    Revisão de fundamentos básicos em Java

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoLancamento = 2023;
+        System.out.println("Ano de lançamento: " + anoLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        //comentários
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de ação com aviões de combate
+                Bom filme!
+                Ano de lançamento
+                """ + anoLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
+
+    }
+}
